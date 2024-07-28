e9d3a624e365ea1a86f37441124bf54b99bee456
Roderick Nagashima <rburkhardt@gmail.com>
2024-07-28 08:08:38 -0700

    Update init structure.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RecipeExplorer
+# Intel471
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.8.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'recipe-explorer' title`, () => {
+  it(`should have the 'intel-471' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recipe-explorer');
+    expect(app.title).toEqual('intel-471');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recipe-explorer');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, intel-471');
   });
 });
